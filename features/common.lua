f31df3acfbe53fb957b1a1aef779da4b7dad7ab5
BrunoMine <borgesdossantosbruno@gmail.com>
2019-12-04 13:49:43 -0300

    Fix code header

--- features/common.lua
+++ features/common.lua
@@ -1,9 +1,9 @@
 --[[
 	Mod HardTorch for Minetest
-	Copyright (C) 2019 BrunoMine (https://github.com/BrunoMine)
+	Copyright (C) 2017-2019 BrunoMine (https://github.com/BrunoMine)
 	
 	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <https://www.gnu.org/licenses/>5.
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 	
 	Common Methods
   ]]
